8865ea7c949f92c8e617423299f5159e01c84ef6
maritmcquaig <marit.mcquaig@gmail.com>
2021-12-04 22:43:37 -0600

    Adding files needed for python code + app update

--- app/app.R
+++ app/app.R
@@ -11,22 +11,22 @@ library(lubridate)
 library(scales)
 
 library(reticulate)
-use_python('/Users/violaliu/opt/anaconda3/bin/python', require=T)
-source_python("/Users/violaliu/Desktop/hw/generate_suggestions.py")
-
+py_install("pandas")
+use_python('/Users/maritmcquaig/Library/r-miniconda/envs/r-reticulate/bin/python', require=T)
+source_python("/Users/maritmcquaig/Documents/GitHub/Group2_628_Module3/generate_suggestions.py")
 
 
 #shop <- 'The Waffle Window'
 #city <- 'Portland'
 #period <- "pre covid"
 #state <- "OR"
-suggestions = main_func(shop, city, state, period)
-length(suggestions)
-good_suggestion = suggestions[0:1]
-bad_suggestion = suggestions[2:2]
+#suggestions = main_func(shop, city, state, period)
+#length(suggestions)
+#good_suggestion = suggestions[0:1]
+#bad_suggestion = suggestions[2:2]
 
-#c <- read.csv("/Users/maritmcquaig/Documents/stat628/module3/covid.csv")
-#pc <- read.csv("/Users/maritmcquaig/Documents/stat628/module3/precovid.csv")
+c <- read.csv("/Users/maritmcquaig/Documents/stat628/module3/covid.csv")
+pc <- read.csv("/Users/maritmcquaig/Documents/stat628/module3/precovid.csv")
 
 pc$date <- format(as.POSIXct(pc$date,format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')
 pcdates <- pc %>% count(date)
@@ -37,14 +37,13 @@ pcdates <- pcdates %>% add_column(covid = "Pre-Covid-19")
 cdates <- cdates %>% add_column(covid = "Covid-19")
 
 alldates <- rbind(pcdates,cdates)
-#ggplot(alldates)+
-#  geom_point(aes(x = as.Date(date), y = n, col = covid))+
-#  geom_vline(xintercept = as.numeric(as.Date('2020-03-01')), col = "black")+
-#  geom_vline(xintercept = as.numeric(as.Date('2020-04-01')), col = "black")+
-#  xlab("Date")+
-#  ylab("Number of Reviews")+
-#  labs(col = "Time Period")
-
+ggplot(alldates)+
+  geom_point(aes(x = as.Date(date), y = n, col = covid))+
+  geom_vline(xintercept = as.numeric(as.Date('2020-03-01')), col = "black")+
+  geom_vline(xintercept = as.numeric(as.Date('2020-04-01')), col = "black")+
+  xlab("Date")+
+  ylab("Number of Reviews")+
+  labs(col = "Time Period")
 
 
 
@@ -58,7 +57,7 @@ sidebar <- dashboardSidebar(
   ),
   sidebarMenu(
     selectInput("city", "Choose a state/province:",
-                choices = c('BC', 'CO', 'FL', 'GA', 'MA', 'OH', 'OR', 'TX', 'WA', 'WY'))
+                choices = c('BC', 'CO', 'FL', 'GA', 'MA', 'OR', 'TX'))
   )
   
 )
@@ -70,8 +69,9 @@ body <- dashboardBody(
   ),
   tabItems(
     tabItem(tabName = "data",
-            h2("Data Description"),
-            img(src = "dates.png", height = '300px', width = '500px')
+            h2("Yelp Reviews of Ice Cream Shops Since March 2019"),
+            fluidRow(box(plotOutput("plot1", height = 250)),
+                     box(h5("We examined reviews from March 2019 through the beginning of 2021. Since shutdowns associated with Covid-19 began in the United States around mid-march, we split our dataset into two around that time. We also removed a one month window of data (3/1/20-4/1/20). ")))
             
     ),
       tabItem(tabName = "results",
@@ -81,7 +81,7 @@ body <- dashboardBody(
     tabItem(tabName = "suggestions",
             h2("Business Suggestions")
     )
-  ),
+  )
 )
 
 
@@ -97,10 +97,15 @@ ui <- dashboardPage(header,
                     footer)
 
 server <- function(input, output, session) {
-  
-
-  
-
+  output$plot1 <- renderPlot({
+    ggplot(alldates)+
+      geom_point(aes(x = as.Date(date), y = n, col = covid))+
+      geom_vline(xintercept = as.numeric(as.Date('2020-03-01')), col = "black")+
+      geom_vline(xintercept = as.numeric(as.Date('2020-04-01')), col = "black")+
+      xlab("Date")+
+      ylab("Number of Reviews")+
+      labs(col = "Time Period")
+  })
 }
 
 
